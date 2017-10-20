2a30fd2cb3b46e7f2e081fcf76db86fd1b1ee745
slin12 <s.lin12@gmail.com>
2017-10-20 15:44:22 -0400

    Done.

---
+++ db/seed.rb
@@ -0,0 +1,5 @@
+Artist.create(name: "Beyonce")
+Artist.create(name: "Fall Out Boy")
+
+Genre.create(name: "Rock")
+Genre.create(name: "Alternative")
--- spec/models/01_song_spec.rb
+++ spec/models/01_song_spec.rb
@@ -1,14 +1,15 @@
-describe 'Song' do
-  before do
-    @song = Song.create(name: "Forever")
-  end
-
-  after do 
-    clean_database
-  end
-
-  it 'has a name' do
-    expect(Song.where(name: "Forever").first).to eq(@song)
-  end
-
-end
+# describe 'Song' do
+#   before do
+#     @song = Song.create(name: "Forever")
+#     @song.artist =
+#   end
+#
+#   after do
+#     clean_database
+#   end
+#
+#   it 'has a name' do
+#     expect(Song.where(name: "Forever").first).to eq(@song)
+#   end
+#
+# end
